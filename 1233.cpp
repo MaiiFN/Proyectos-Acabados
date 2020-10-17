07bd9c521863b46df71a6642e27b4020c01579b1
Maii <64509256+MaiiFN@users.noreply.github.com>
2020-10-17 16:06:48 -0500

    Examen Programacion 1sem

---
+++ 1233.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+
+
+	return 0;
+}
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	//variables
+//	int refresco = 10;
+//	int papas = 12;
+//	int chilaquiles = 20;
+//	int torta = 25;
+//	int nieve = 15;
+//
+//	int promocion1 = 40;
+//	//Pongo las variables separadas para una mejor lectura de un tercero
+//	//en vez de que se ponga todo junto.
+//
+//	int dinero;
+//
+//	//Nos pregunta cuando dinero tenemos
+//	cout << " Cuanto dinero tenemos: "; cin >> dinero;
+//
+//	//Nos pregunta el senior de la tienda
+//	cout << " S- Que gusta llevarse joven?"<<endl;
+//	cout << "\n Tenemos:" << " \n 1- Refresco: " << refresco << "\n 2- Papas: " << papas << "\n 3- Chilaquiles: " << chilaquiles << "\n 4- Tortas: " << torta <<"\n 5- Nieves: "<< nieve << endl;
+//	
+//
+//
+//	switch (dinero) {
+//
+//	case 1:	if (dinero <= 40 && dinero >= 30) { cout << "Hola"; }
+//
+//
+//
+//
+//		break;
+//	}
+//	
+//
+//
+//	return 0;
+//} // Final del main
